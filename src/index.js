69ff60cf3e3b6972ba0dfdf031021c0e7bb59056
mariam-ship-it118 <22-101170@students.eui.edu.eg>
2026-04-20 00:11:38 +0200

    apk added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Updated for React 18+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import RoutingApp from './RoutingApp';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RoutingApp />
-  </React.StrictMode>
-);
+// 1. Uncomment and adjust the path below if you are actually using a service worker.
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+// 2. Uncomment if you intend to render RoutingApp instead of App.
+// import RoutingApp from './RoutingApp'; 
+
+const startApp = () => {
+  // 3. React 18 requires createRoot instead of ReactDOM.render
+  const container = document.getElementById('root');
+  const root = createRoot(container);
+  
+  root.render(
+    <React.StrictMode>
+      <RoutingApp />
+    </React.StrictMode>
+  );
+
+  // 4. Call your service worker here if you imported it above
+  // serviceWorkerRegistration.register();
+}
+
+// 5. Cordova check stays the same
+if (window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else {
+  startApp();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
